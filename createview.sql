248fadefad28617b99bd6e09502821daea109dcc
michael-rosten <32375371+michael-rosten@users.noreply.github.com>
2021-01-14 16:31:43 -0800

    Delete createview.sql

--- createview.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW viewComputedCount(speciesID,computedAnimalCount) AS
-	SELECT  DISTINCT speciesID, COUNT(speciesID) as computedAnimalCount
-	FROM Animals
-	GROUP BY speciesID
-	HAVING computedAnimalCount > 2;

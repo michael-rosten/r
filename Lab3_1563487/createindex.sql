2d66a8adcb6403d62996aca12b621f96c7770bf6
michael-rosten <32375371+michael-rosten@users.noreply.github.com>
2021-01-14 16:35:51 -0800

    Delete Lab3_1563487 directory

--- Lab3_1563487/createindex.sql
+++
@@ -1,1 +0,0 @@
-CREATE INDEX FindMembers ON Members(name, address);
--- Lab3_1563487/general.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE Species
-	ADD CONSTRAINT ani CHECK (animalCount >= 0);
-ALTER TABLE Members
-	ADD CONSTRAINT b CHECK (expirationDate > joinDate);
-ALTER TABLE Keepers
-	ADD CONSTRAINT c CHECK (keeperLevel = NULL) AND (keeperSalary = NULL)
-		OR (keeperLevel != NULL) AND (keeperSalary != NULL);
